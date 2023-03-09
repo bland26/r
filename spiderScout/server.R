dbc54d923901f2b7e7b853e5e155f6354e55ae6b
bland26 <bland26@gmail.com>
2023-03-09 16:29:21 -0600

    Update server.R

--- spiderScout/server.R
+++ spiderScout/server.R
@@ -1,23 +1,18 @@
-data <- read.csv("Rtesting - Magnolia Regional.csv")
 
 function(input, output, session) {
-    
-
 
     selectedData1 <- reactive({
-        data %>%
-            filter(team %in% input$teams )
+        scoutData %>%
+            filter(robot %in% input$teams )
     })
     
     selectedData2 <- reactive({
         selectedData1() %>%
-            select(4:17) 
+            select(6,10,14,15,17:19) 
     })
     
-    
 
-    
-    
+
     # Combine the selected variables into a new data frame
     output$plot1 <- renderPlotly({
         
@@ -28,98 +23,98 @@ function(input, output, session) {
         ) %>%
             add_trace(
                 r = as.matrix(selectedData2()[1,]) ,
-                theta = c("ACH","ACM","ACL","ACuH","ACuM","ACuL","AChL",
-                          "TCH","TCM","TCL","TCuH","TCuM","TCuL","TChL"),
+                theta = c("Auto Movement", "Auto Node Score", "Tele Node Score", "Links (Alliance)"
+                          , "Charge Station Tele", "Driver Skill", "Collaboration"),
                 showlegend = TRUE,
                 mode = "markers",
-                name = selectedData1()[1,1]
+                name = selectedData1()[1,4]
             ) %>%
             add_trace(
                 r = as.matrix(selectedData2()[2,]),
-                theta = c("ACH","ACM","ACL","ACuH","ACuM","ACuL","AChL",
-                          "TCH","TCM","TCL","TCuH","TCuM","TCuL","TChL"),
+                theta = c("Auto Movement", "Auto Node Score", "Tele Node Score", "Links (Alliance)"
+                          , "Charge Station Tele", "Driver Skill", "Collaboration"),
                 showlegend = TRUE,
                 mode = "markers",
                 visible="legendonly",
-                name = selectedData1()[2,1]
+                name = selectedData1()[2,4]
             ) %>%
             add_trace(
                 r = as.matrix(selectedData2()[3,]),
-                theta = c("ACH","ACM","ACL","ACuH","ACuM","ACuL","AChL",
-                          "TCH","TCM","TCL","TCuH","TCuM","TCuL","TChL"),
+                theta = c("Auto Movement", "Auto Node Score", "Tele Node Score", "Links (Alliance)"
+                          , "Charge Station Tele", "Driver Skill", "Collaboration"),
                 showlegend = TRUE,
                 mode = "markers",
                 visible="legendonly",
-                name = selectedData1()[3,1]
+                name = selectedData1()[3,4]
             ) %>%
             add_trace(
                 r = as.matrix(selectedData2()[4,]),
-                theta = c("ACH","ACM","ACL","ACuH","ACuM","ACuL","AChL",
-                          "TCH","TCM","TCL","TCuH","TCuM","TCuL","TChL"),
+                theta = c("Auto Movement", "Auto Node Score", "Tele Node Score", "Links (Alliance)"
+                          , "Charge Station Tele", "Driver Skill", "Collaboration"),
                 showlegend = TRUE,
                 mode = "markers",
                 visible="legendonly",
-                name = selectedData1()[4,1]
+                name = selectedData1()[4,4]
             ) %>%
             add_trace(
                 r = as.matrix(selectedData2()[5,]),
-                theta = c("ACH","ACM","ACL","ACuH","ACuM","ACuL","AChL",
-                          "TCH","TCM","TCL","TCuH","TCuM","TCuL","TChL"),
+                theta = c("Auto Movement", "Auto Node Score", "Tele Node Score", "Links (Alliance)"
+                          , "Charge Station Tele", "Driver Skill", "Collaboration"),
                 showlegend = TRUE,
                 mode = "markers",
                 visible="legendonly",
-                name = selectedData1()[5,1]
+                name = selectedData1()[5,4]
             ) %>%
             add_trace(
                 r = as.matrix(selectedData2()[6,]),
-                theta = c("ACH","ACM","ACL","ACuH","ACuM","ACuL","AChL",
-                          "TCH","TCM","TCL","TCuH","TCuM","TCuL","TChL"),
+                theta = c("Auto Movement", "Auto Node Score", "Tele Node Score", "Links (Alliance)"
+                          , "Charge Station Tele", "Driver Skill", "Collaboration"),
                 showlegend = TRUE,
                 mode = "markers",
                 visible="legendonly",
-                name = selectedData1()[6,1]
+                name = selectedData1()[6,4]
             ) %>%
             add_trace(
                 r = as.matrix(selectedData2()[7,]),
-                theta = c("ACH","ACM","ACL","ACuH","ACuM","ACuL","AChL",
-                          "TCH","TCM","TCL","TCuH","TCuM","TCuL","TChL"),
+                theta = c("Auto Movement", "Auto Node Score", "Tele Node Score", "Links (Alliance)"
+                          , "Charge Station Tele", "Driver Skill", "Collaboration"),
                 showlegend = TRUE,
                 mode = "markers",
                 visible="legendonly",
-                name = selectedData1()[7,1]
+                name = selectedData1()[7,4]
             ) %>%
             add_trace(
                 r = as.matrix(selectedData2()[8,]),
-                theta = c("ACH","ACM","ACL","ACuH","ACuM","ACuL","AChL",
-                          "TCH","TCM","TCL","TCuH","TCuM","TCuL","TChL"),
+                theta = c("Auto Movement", "Auto Node Score", "Tele Node Score", "Links (Alliance)"
+                          , "Charge Station Tele", "Driver Skill", "Collaboration"),
                 showlegend = TRUE,
                 mode = "markers",
                 visible="legendonly",
-                name = selectedData1()[8,1]
+                name = selectedData1()[8,4]
             ) %>%
             add_trace(
                 r = as.matrix(selectedData2()[9,]),
-                theta = c("ACH","ACM","ACL","ACuH","ACuM","ACuL","AChL",
-                          "TCH","TCM","TCL","TCuH","TCuM","TCuL","TChL"),
+                theta = c("Auto Movement", "Auto Node Score", "Tele Node Score", "Links (Alliance)"
+                          , "Charge Station Tele", "Driver Skill", "Collaboration"),
                 showlegend = TRUE,
                 mode = "markers",
                 visible="legendonly",
-                name = selectedData1()[9,1]
+                name = selectedData1()[9,4]
             ) %>%
             add_trace(
                 r = as.matrix(selectedData2()[10,]),
-                theta = c("ACH","ACM","ACL","ACuH","ACuM","ACuL","AChL",
-                          "TCH","TCM","TCL","TCuH","TCuM","TCuL","TChL"),
+                theta = c("Auto Movement", "Auto Node Score", "Tele Node Score", "Links (Alliance)"
+                          , "Charge Station Tele", "Driver Skill", "Collaboration"),
                 showlegend = TRUE,
                 mode = "markers",
                 visible="legendonly",
-                name = selectedData1()[10,1]
+                name = selectedData1()[10,4]
             ) %>%
             layout(
                 polar = list(
                     radialaxis = list(
                         visible = T,
-                        range = c(-1,5)
+                        range = c(-0.25,1)
                     )
                 ),
                 
@@ -129,8 +124,45 @@ function(input, output, session) {
             )
         
     })
+    selectedData3 <- reactive({
+        scoutData %>%
+            filter(robot %in% input$team )
+    })
     
+    at <- reactive({input$time})
+    
+    
+    selectedData4 <- reactive({selectedData1 %>%
+            select(7:9)
+    })
+    selectedData5 <- reactive({selectedData1 %>%
+            select(11:13)
+    })
+    output$heatAuto <- renderPlotly({
+        plot_ly(
+            x = c(1:9), y = c("Low","Mid", "High"),
+            z = scoutData3, type = "heatmap",
+        )%>%
+            layout(title = "Auto")
+    })
+    output$heatTele <- renderPlotly({
+        plot_ly(
+            x = c(1:9), y = c("Low","Mid", "High"),
+            z = scoutData3, type = "heatmap",
+        ) %>%
+            layout(title = "Tele")
+    })
+    
+    
+   
+    
+    winData <- reactive({
+        getTeamMatchesCustom(2023,input$a)
+    })
+    
+    output$mr <- renderPlot({
+        ggplot(winData(), aes(x = Alliance_Score, y = Opponent_Score)) + 
+            geom_point(aes(color = factor(win),shape = factor(str_trunc(name, 20)), size = 1))+ 
+            xlim(10,200) + ylim(10,200) + labs (shape = "Event") + labs (color = "Results")
+    })
 }
-
-
-
